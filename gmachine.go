7b78c32671a60a762d2a29644095cd203ee86080
Zachary Proser <zackproser@gmail.com>
2023-05-02 12:50:06 -0400

    Init to win it

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,31 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+const (
+	HaltOpcode = 0
+	NoOpcode   = 1
+)
+
+type machine struct {
+	P      uint64
+	Memory map[uint64]uint64
+}
+
+func New() *machine {
+	return &machine{
+		P:      0,
+		Memory: make(map[uint64]uint64),
+	}
+}
+
+// TODO - want to be able to use string OPCODES ("HALT")
+func (m *machine) Run() {
+	for {
+		current := m.Memory[m.P]
+		if current == HaltOpcode {
+			return
+		}
+		m.P++
+	}
+}
--- gmachine_test.go
+++ gmachine_test.go
@@ -8,11 +8,6 @@ import (
 func TestNew(t *testing.T) {
 	t.Parallel()
 	g := gmachine.New()
-	wantMemSize := gmachine.DefaultMemSize
-	gotMemSize := len(g.Memory)
-	if wantMemSize != gotMemSize {
-		t.Errorf("want %d words of memory, got %d", wantMemSize, gotMemSize)
-	}
 	var wantP uint64 = 0
 	if wantP != g.P {
 		t.Errorf("want initial P value %d, got %d", wantP, g.P)
@@ -21,5 +16,20 @@ func TestNew(t *testing.T) {
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
+	}
+}
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+
+	// Add ADD instruction to our current machine state
+	g.Memory[0] = gmachine.NoOpcode
+	g.Memory[1] = gmachine.NoOpcode
+
+	g.Run()
+
+	if g.P != 2 {
+		t.Errorf("Wanted 2 but got %+v\n", g.P)
 	}
 }
